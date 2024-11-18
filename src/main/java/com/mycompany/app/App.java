86612dcd111a5ae525606de98bab872c2fffb816
Lucian Zhao <l32zhao@uwaterloo.ca>
2024-11-17 22:27:22 -0500

    fix bug

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,8 +8,6 @@ public class App {
     //@ non_null
     private static final String MESSAGE = "Hello World!";
 
-    //@ invariant MESSAGE != null && MESSAGE instanceof CharSequence;
-
     public App() {}
 
     public static void main(String[] args) {
